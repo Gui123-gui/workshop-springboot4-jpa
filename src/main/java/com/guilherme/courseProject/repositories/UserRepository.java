e35184baa4791b4f7deb7ddc4f42aed45b3b6aab
SeuNome <gui_marini123@hotmail.com>
2026-03-31 21:09:36 -0300

    Service layer, component registration

--- src/main/java/com/guilherme/courseProject/repositories/UserRepository.java
+++ src/main/java/com/guilherme/courseProject/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.guilherme.courseProject.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.guilherme.courseProject.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
